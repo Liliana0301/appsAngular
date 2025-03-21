f4da5b49da8780cdb6fc8d36ce6a6574e980a0ae
deysi <deysisolorzano0301@gmail.com>
2025-03-21 11:15:58 -0600

    primer commit -subiendo proyectos

--- src/app/services/aeropuertos.service.ts
+++ src/app/services/aeropuertos.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Aeropuerto } from '../models/aeropuerto.models';
+import { environment } from '../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AeropuertosService {
 
-  constructor() { }
+  private apiUrl: string = environment.apiUrl +'aerolinea/';
+  constructor(private http: HttpClient){ }
+    getAerolineas():Observable<Aeropuerto[]>{
+      return this.http.get<Aeropuerto[]>(this.apiUrl);
+
+    }
+    createAerolinea(aerolinea: Aeropuerto):Observable<Aeropuerto>{
+      return this.http.post<Aeropuerto>(`${this.apiUrl}`,aerolinea);
+    }
+
+    
+    updateAerolinea(aerolinea: Aeropuerto): Observable<Aeropuerto>{
+      return this.http.put<Aeropuerto>(`${this.apiUrl}${aerolinea.id}`, aerolinea);
+    }
+    
+    deleteAerolinea(idAerolinea: number):Observable<Aeropuerto>{
+      return this.http.delete<Aeropuerto>(`${this.apiUrl}${idAerolinea}`);
+    
+    }
+
 }
